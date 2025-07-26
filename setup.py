cfd64da98bbf91e81eb891e6ab421dcae554cfc0
BrotatoBoiV2 <flamingburrito15@gmail.com>
2025-07-26 13:47:22 -0400

    Updated the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PotatoEngine",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "pygame>=2.6.1"
